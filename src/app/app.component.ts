6242beffdecf6c4bb4255a8ed662135521dd6be7
sitt hmue eain <yoopajdoto@gmail.com>
2023-05-04 09:20:37 +0630

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,33 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgwWowService } from 'ngx-wow';
 
+const navEl = document.querySelector('.navbar');
+window.addEventListener('scroll',()=>{
+  if(window.scrollY >= 56){
+    console.log('front');
+    
+    navEl?.classList.add('navbar-scrolled')
+  }else if(window.scrollY < 56){
+    console.log('back');
+    
+    navEl?.classList.remove('navbar-scrolled')
+  }
+})
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit{
   constructor(private wowService : NgwWowService){
     this.wowService.init();
   }
   title = 'GocSeaFood';
   public isCollapsed = true;
   
+  ngOnInit(): void {
+  }
   }
   
   
